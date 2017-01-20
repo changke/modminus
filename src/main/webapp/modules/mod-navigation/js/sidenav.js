6d42449dd69f6b1ad3f7c2ea068fac60096e499b
Ke Chang <ke.chang@1und1.de>
2017-01-20 14:45:48 +0100

    side nav toggling; icon-only btns

--- src/main/webapp/modules/mod-navigation/js/sidenav.js
+++ src/main/webapp/modules/mod-navigation/js/sidenav.js
@@ -15,9 +15,35 @@ goog.require('mm.module');
 mm.modules.sidenav = function(ctx, id, pubsub, params) {
   goog.base(this, ctx, id, pubsub, params);
   this.name = 'sidenav';
+
+  this.topNavLinkClass = this.name + '__link';
+  this.subWrapperClass = this.name + '__subwrapper';
+  this.openClass = 'sidenav__subwrapper_open';
 };
 goog.inherits(mm.modules.sidenav, mm.module);
 goog.exportSymbol('mm.modules.sidenav', mm.modules.sidenav);
 
 mm.modules.sidenav.prototype.run = function() {
+  var self = this;
+
+  var topNavLinks = this.ctx.find('.' + this.topNavLinkClass);
+  topNavLinks.on('click', function(e) {
+    e.preventDefault();
+
+    self.hideAllSubNavs();
+
+    var sectionId = this.getAttribute('data-section');
+    if (sectionId && sectionId.length > 0) {
+      var subNav = self.ctx.find('.' + self.subWrapperClass + '[data-section="' + sectionId + '"]');
+      if (subNav.length > 0) {
+        subNav.addClass(self.openClass);
+      }
+    }
+    return false;
+  });
+};
+
+mm.modules.sidenav.prototype.hideAllSubNavs = function() {
+  var subNavs = this.ctx.find('.' + this.subWrapperClass);
+  subNavs.removeClass(this.openClass);
 };
