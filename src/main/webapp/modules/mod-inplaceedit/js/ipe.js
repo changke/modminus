440b8b42f1d26f34532d621269670dfb4a9708ac
Ke Chang <ke.chang@1und1.de>
2017-01-30 16:58:08 +0100

    more functions in ipe

--- src/main/webapp/modules/mod-inplaceedit/js/ipe.js
+++ src/main/webapp/modules/mod-inplaceedit/js/ipe.js
@@ -41,6 +41,77 @@ mm.modules.ipe.prototype.listItems = function(itemsData) {
   // set edit/delete button events
   var items = this.ctx.find(this.itemSelector);
   if (items.length > 0 && items.length === itemsData.length) {
-    for (var i = 0; i < items.length; i++) {}
+    // event for delete
+    var deleteBtns = items.find(this.deleteButtonSelector);
+    this.applyEvent(deleteBtns, 'delete');
+
+    var editBtns = items.find(this.editButtonSelector);
+    this.applyEvent(editBtns, 'edit');
   }
+};
+
+mm.modules.ipe.prototype.applyEvent = function(elems, eventName) {
+  var self = this;
+
+  switch (eventName) {
+    case 'delete':
+      elems.on('click', function(e) {
+        e.preventDefault();
+        var btn = e.currentTarget;
+        var itemId = btn.getAttribute('rel');
+        self.deleteItem(itemId);
+      });
+      break;
+    case 'edit':
+      elems.on('click', function(e) {
+        e.preventDefault();
+        var btn = e.currentTarget;
+        var itemRow = btn.parentNode.parentNode;
+        var item = {
+          id: btn.getAttribute('rel'),
+          title: itemRow.querySelector('.ipe__items h4').innerHTML,
+          description: itemRow.querySelector('.ipe__items p').innerHTML
+        };
+        self.applyEditingMask(itemRow, item);
+      });
+      break;
+    case 'done':
+      elems.on('click', function(e) {
+        e.preventDefault();
+        var btn = e.currentTarget;
+        var itemRow = btn.parentNode.parentNode;
+        var newItem = {
+          id: btn.getAttribute('rel'),
+          title: itemRow.querySelector('[name="txtCateTitle"]').value,
+          description: itemRow.querySelector('[name="txtCateDesc"]').value
+        };
+
+        self.updateItem(newItem);
+      });
+      break;
+  }
+
+};
+
+mm.modules.ipe.prototype.deleteItem = function(itemId) {
+  if (window.confirm('Are you sure?')) {
+    this.log('To delete item ' + itemId + '.');
+  }
+};
+
+mm.modules.ipe.prototype.updateItem = function(newItem) {
+  this.log(JSON.stringify(newItem));
+};
+
+mm.modules.ipe.prototype.applyEditingMask = function(row, item) {
+  var rowHtml = mm.templates.inplaceedit.editingMask({item: item}).content;
+  var oldHtml = row.innerHTML;
+
+  // show edit mask
+  row.innerHTML = rowHtml;
+
+  var doneBtn = row.querySelector(this.doneButtonSelector);
+  this.applyEvent(G(doneBtn), 'done');
+
+  var cancelBtn = row.querySelector(this.cancelButtonSelector);
 };
